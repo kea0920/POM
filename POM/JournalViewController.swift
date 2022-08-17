8c44626debdfa9699d7976f3d4e1f9fbd1e877ed
kea <kea@bxscience.edu>
2022-08-17 12:15:37 -0400

    completed basic outline of app

---
+++ POM/JournalViewController.swift
@@ -0,0 +1,29 @@
+//
+//  JournalViewController.swift
+//  POM
+//
+//  Created by wenjuan Qiu on 8/17/22.
+//
+
+import UIKit
+
+class JournalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ POM/ToDo List/ToDo.swift
@@ -0,0 +1,12 @@
+//
+//  ToDo.swift
+//  POM
+//
+//  Created by wenjuan Qiu on 8/17/22.
+//
+
+import UIKit
+
+class ToDo {
+    
+}
