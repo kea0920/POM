ce679dec2d5cd71f045fe7f9714d99fb8ba2a883
Anginel <anginelmatias@hotmail.com>
2022-08-18 16:07:15 -0400

    adding todolist

---
+++ POM/ToDoListItem/ToDoListItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListItem+CoreDataClass.swift
+//  POM
+//
+//  Created by Anginel on 8/18/22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoListItem)
+public class ToDoListItem: NSManagedObject {
+
+}
---
+++ POM/ToDoListItem/ToDoListItem+CoreDataProperties.swift
@@ -0,0 +1,26 @@
+//
+//  ToDoListItem+CoreDataProperties.swift
+//  POM
+//
+//  Created by Anginel on 8/18/22.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension ToDoListItem {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<ToDoListItem> {
+        return NSFetchRequest<ToDoListItem>(entityName: "ToDoListItem")
+    }
+
+    @NSManaged public var name: String?
+    @NSManaged public var createAt: Date?
+
+}
+
+extension ToDoListItem : Identifiable {
+
+}
