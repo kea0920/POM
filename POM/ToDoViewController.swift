5809a5e26218965fcc8c2e237d99aeb0069f19a5
Anginel <anginelmatias@hotmail.com>
2022-08-18 19:19:39 -0400

    Todo core data updated

--- POM/ToDoViewController.swift
+++ POM/ToDoViewController.swift
@@ -7,23 +7,87 @@
 
 import UIKit
 
-class ToDoViewController: UIViewController {
+class ToDoViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    let context = (UIApplication.shared.delegate as!AppDelegate).persistentContainer.viewContext
+    
+    let tableView: UITableView = {
+    let table = UITableView()
+        table.register(UITableViewCell
+.self, forCellReuseIdentifier: "cell")
+        return table
+        
+    }()
+    
+    private var models = [ToDoListItem]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+       title = "To Do list"
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.frame = view.bounds
+        
+}
+ 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models.count
+}
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let model = models[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = model.name
+        return cell
+    }
+//Core Data
+    
+func getAllItems() {
+        do {
+            models = try context.fetch(ToDoListItem.fetchRequest())
+            
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+        catch {
+            //error
+        }
+    }
 
-        // Do any additional setup after loading the view.
+    func createItem(name: String) {
+        let newItem = ToDoListItem(context: context)
+        newItem.name = name
+        newItem.createAt = Date()
+        do {
+            try context.save()
+        }
+        catch {
+            
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+func deleteItem(item: ToDoListItem) {
+        context.delete(item)
+        do {
+            try context.save()
+        }
+        catch {
+            
+        }
     }
-    */
-
+    
+    func updateItem(item: ToDoListItem, newName: String){
+        item.name = newName
+        do {
+            try context.save()
+        }
+        catch {
+            
+        }
+        
+    }
 }
+
+
